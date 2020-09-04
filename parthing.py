a02d8bd7123d15cdb9af04eedf440b6d43c72aac
MihailUse <alkaevmih@gmail.com>
2020-09-04 23:31:39 +0300

    it works and it's amazing (all pages)

--- parthing.py
+++ parthing.py
@@ -1,64 +1,66 @@
 import requests
 import csv
+import time
 from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
 
-import time
 start_time = time.time()
 
-url = 'https://spb.hh.ru/search/vacancy?area=2&clusters=true&enable_snippets=true&specialization=1&only_with_salary=true&from=cluster_compensation&showClusters=true&page=0'  # url для страницы
+max_amount_pages = int(input('Ограничение страниц: '))
+
+data = ['vacancy', 'vacancy_link', 'skills']  # columns
+
+try:
+    with open('parthing.csv', 'w') as f:
+        writer = csv.writer(f, delimiter=';')
+        writer.writerow(data)
+except:
+    exit('Что-то пошло не так')
+
+url = 'https://spb.hh.ru/catalog/Informacionnye-tehnologii-Internet-Telekom'  # url для страницы
 r = requests.get(url, headers={'User-Agent': UserAgent().chrome}).text
 soup = BeautifulSoup(r, 'html.parser')
 
-all_vacancy = soup.find('div', class_='vacancy-serp').find_all('a', class_='bloko-link HH-LinkModifier')
-all_link = soup.find('div', class_='vacancy-serp').find_all('a', class_='bloko-link HH-LinkModifier', href=True)
-all_pay = soup.find('div', class_='vacancy-serp').find_all('span', {'data-qa': 'vacancy-serp__vacancy-compensation'})
+page_count = int(soup.find_all('span', class_='pager-item-not-in-short-range')[2].find('a').get_text())  # оооооооооооооооооочень криво
+print('Количество cтраниц: ', page_count)
 
-page_len = soup.find_all('span', class_='pager-item-not-in-short-range')[2].find('a').text  # оооооооооооооооооочень криво
-print('Количество cтраниц: ', page_len)
-for i in range(int(page_len) - 1):
-    url = 'https://spb.hh.ru/search/vacancy?area=2&clusters=true&enable_snippets=true&specialization=1&only_with_salary=true&from=cluster_compensation&showClusters=true&page={i}'  # url для страницы
+# перебор кождой страницы
+for page in range(page_count):
+    # ограничение обрабатываемых страниц
+    if page == max_amount_pages:
+        break
+
+    url = 'https://spb.hh.ru/catalog/Informacionnye-tehnologii-Internet-Telekom?page={page}'  # url для страницы
     r = requests.get(url, headers={'User-Agent': UserAgent().chrome}).text
     soup = BeautifulSoup(r, 'html.parser')
 
-    # массивы должны быть большими, очень большими...
-    all_vacancy += (soup.find('div', class_='vacancy-serp').find_all('a', class_='bloko-link HH-LinkModifier'))
-    all_link += (soup.find('div', class_='vacancy-serp').find_all('a', class_='bloko-link HH-LinkModifier', href=True))
-    all_pay += (soup.find('div', class_='vacancy-serp').find_all('span', {'data-qa': 'vacancy-serp__vacancy-compensation'}))
-    print('button exists on this page: ', i + 1)
-    i += 1
+    posts = soup.find('div', class_='vacancy-serp').find_all('div', {'class': 'vacancy-serp-item'})
+    print('Количество постов на странице: ', len(posts))
 
-    # ограничение страниц
-    if i == 5:
-        break
+    # перебор каждого поста и добавление в файл
+    for post in range(len(posts)):
+        vacancy = posts[post].find('a', class_='bloko-link HH-LinkModifier').text
+        vacancy_link = posts[post].find('a', class_='bloko-link HH-LinkModifier', href=True)['href']
+        # print(post + 1, vacancy, vacancy_link)
 
-data = [['name_vacancy', 'vacancy_link', 'pay', 'skills']]  # columns
+        try:
+            r = requests.get(vacancy_link, headers={'User-Agent': UserAgent().chrome}).text
+            soup = BeautifulSoup(r, 'html.parser')
 
-for el in range(len(all_vacancy)):
-    vacancy = all_vacancy[el].get_text()
-    link = all_link[el]['href']
-    pay = all_pay[el].get_text()
+            all_skills = soup.find('div', class_='bloko-tag-list').find_all('span', {'data-qa': 'bloko-tag__text'})
+            skills = []
+            for el in range(len(all_skills)):
+                skills.append(all_skills[el].text)
+            skills = ', '.join(skills)
+        except:
+            skills = 'Не удалось получить доступ к вакансии или же теги не указаны'
 
-    r = requests.get(link, headers={'User-Agent': UserAgent().chrome}).text
-    soup = BeautifulSoup(r, 'html.parser')
+        result = [vacancy, vacancy_link, skills]
 
-    try:
-        all_skills = soup.find('div', class_='bloko-tag-list')\
-            .find_all('span', {'data-qa': 'bloko-tag__text'})
-        skills = []
-        for i in range(len(all_skills)):
-            skills.append(all_skills[i].get_text())
-        skills = ', '.join(skills)
-    except:
-        skills = 'Ключевые навыки не указаны'
+        with open('parthing.csv', 'a') as f:
+            writer = csv.writer(f, delimiter=';')
+            writer.writerow(result)
 
-    result = [vacancy, link, pay, skills]
-    data.append(result)
-
-# запись в csv файл
-with open('hhh.csv', 'w') as f:
-    writer = csv.writer(f, delimiter=';')
-    for row in data:
-        writer.writerow(row)
+    print("--- page load time %s seconds ---" % (time.time() - start_time))
 
 print("--- execution time %s seconds ---" % (time.time() - start_time))
